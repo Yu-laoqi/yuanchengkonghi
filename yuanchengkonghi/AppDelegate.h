27a2b3b068fe7043e64efd6fb26f62b4ef648477
Yu-laoqi <1769497542@qq.com>
2018-08-03 15:29:24 +0800

    Initial Commit

---
+++ yuanchengkonghi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  yuanchengkonghi
+//
+//  Created by 于泽峰 on 2018/8/3.
+//  Copyright © 2018年 BW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
